34cb821c6e6f5b3ad44e5524a858d30f0ba85450
RizkiB12 <rbagus306@gmail.com>
2022-05-17 18:11:45 +0700

    add relation between author and book

--- models/author.js
+++ models/author.js
@@ -10,7 +10,11 @@ module.exports = (sequelize, DataTypes) => {
      * The `models/index` file will call this method automatically.
      */
     static associate(models) {
-      // define association here
+      Author.hasMany(models.Book, {
+        foreignKey: 'authorId',
+        as: 'book'
+        // define association here
+      });
     }
   }
   Author.init({
--- models/book.js
+++ models/book.js
@@ -10,7 +10,11 @@ module.exports = (sequelize, DataTypes) => {
      * The `models/index` file will call this method automatically.
      */
     static associate(models) {
+      Book.belongsTo(models.Author, {
+        foreignKey: 'authorId',
+        as: 'author'
       // define association here
+      });
     }
   }
   Book.init({
